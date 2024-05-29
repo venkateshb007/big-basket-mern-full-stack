1913085a77098fce7b94f5d522297bba06c7a3c4
Venkatesh B <venkateshb698@gmail.com>
2024-05-29 20:57:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+ import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark navbar-expand-sm bg-primary">
+        <a href="/" className="navbar-brand">
+          Big-Basket
+        </a>
+      </nav>
+
+    </div>
+  );
+};
+
+export default App;
